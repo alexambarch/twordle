bb144438f536900ca7d3d34df64196e1ff21cf3c
Alex Ambarchian <alex@alexambarchian.com>
2022-01-21 01:46:37 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject twordle "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject twordle "0.1.0"
+  :description "Solve today's Wordle using the power of Twitter (kinda)"
+  :url "https://github.com/alexambarch/twordle"
+  :license {:name "MIT License"
+            :url "https://mit-license.org"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [clj-http "3.12.3"]
                  [org.clojure/data.json "2.4.0"]]
